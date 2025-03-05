0a17abdb72866c34a06e4d57d0815ebdd5962ab0
YAJNESH AT <yaat21ec@cmrit.ac.in>
2025-03-05 14:31:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/plan-trip" element={<PlanTrip />} />
-          <Route path="/about" element={<About />} />
+       
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
